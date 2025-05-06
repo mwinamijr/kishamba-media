44d095d1c1b39fc5987791fe2e7604826e17b242
mwinamijr <mwinamijr@gmail.com>
2025-05-06 17:00:02 +0300

    added followers component and added all the components to the home page

---
+++ src/components/home/Followus.js
@@ -0,0 +1,71 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const socialLinks = [
+  {
+    name: "Facebook",
+    color: "#39569E",
+    icon: "fab fa-facebook-f",
+    text: "12,345 Fans",
+    link: "https://facebook.com/kishambamedia",
+  },
+  {
+    name: "Twitter",
+    color: "#52AAF4",
+    icon: "fab fa-twitter",
+    text: "12,345 Followers",
+    link: "https://twitter.com/kishambamedia",
+  },
+  {
+    name: "LinkedIn",
+    color: "#0185AE",
+    icon: "fab fa-linkedin-in",
+    text: "12,345 Connects",
+    link: "https://linkedin.com/kishambamedia",
+  },
+  {
+    name: "Instagram",
+    color: "#C8359D",
+    icon: "fab fa-instagram",
+    text: "12,345 Followers",
+    link: "https://instagram.com/kishambamedia",
+  },
+  {
+    name: "YouTube",
+    color: "#DC472E",
+    icon: "fab fa-youtube",
+    text: "12,345 Subscribers",
+    link: "https://youtube.com/@kishambamedia",
+  },
+];
+
+const FollowUs = () => {
+  return (
+    <div className="mb-3">
+      <div className="section-title mb-0">
+        <h4 className="m-0 text-uppercase font-weight-bold">Follow Us</h4>
+      </div>
+      <div className="bg-white border border-top-0 p-3">
+        {socialLinks.map((socialLink, index) => (
+          <Link
+            key={index}
+            to={socialLink.link}
+            className="d-block w-100 text-white text-decoration-none mb-3"
+            style={{ background: socialLink.color }}
+          >
+            <i
+              className={`${socialLink.icon} text-center py-4 mr-3`}
+              style={{
+                width: "65px",
+                background: "rgba(0, 0, 0, 0.2)",
+              }}
+            ></i>
+            <span className="font-weight-medium">{socialLink.text}</span>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default FollowUs;
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -5,6 +5,23 @@ import NewsCarousel from "../components/home/NewsSlider";
 import BreakingNewsTicker from "../components/home/BreakingNews";
 import TopFeature from "../components/home/TopFeature";
 import FeaturedNews from "../components/home/FeaturedNews";
+import LatestNews from "../components/home/LatestNews";
+import FollowUs from "../components/home/Followus";
+import TrendingNews from "../components/home/TrendingNews";
+import WhatsNew from "../components/home/WhatsNew";
+
+const tags = [
+  "Politics",
+  "Business",
+  "Corporate",
+  "Business",
+  "Health",
+  "Education",
+  "Science",
+  "Business",
+  "Foods",
+  "Travel",
+];
 
 const Home = () => {
   return (
@@ -19,237 +36,57 @@ const Home = () => {
       <NewsCarousel />
       <BreakingNewsTicker />
       <TopFeature />
-      <FeaturedNews />
 
-      <br />
-      <div className="row mb-2">
-        <div className="col-sm-6">
-          <div className="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-            <div className="col p-4 d-flex flex-column position-static">
-              <strong className="d-inline-block mb-2 text-primary">
-                World
-              </strong>
-              <h3 className="mb-0">Featured post</h3>
-              <div className="mb-1 text-muted">Nov 12</div>
-              <p className="card-text mb-auto">
-                This is a wider card with supporting text below as a natural
-                lead-in to additional content.
-              </p>
-              <Link to="#" className="stretched-link">
-                Continue reading
-              </Link>
+      <div className="container-fluid">
+        <FeaturedNews />
+        <div className="container py-5">
+          <div className="row">
+            <div className="col-lg-8">
+              <WhatsNew />
+              <LatestNews />
             </div>
-            <div className="col-auto d-none d-lg-block">
-              <svg
-                className="bd-placeholder-img"
-                width="200"
-                height="250"
-                xmlns="http://www.w3.org/2000/svg"
-                role="img"
-                aria-label="Placeholder: Thumbnail"
-                preserveAspectRatio="xMidYMid slice"
-                focusable="false"
-              >
-                <title>Placeholder</title>
-                <rect width="100%" height="100%" fill="#55595c" />
-                <text x="50%" y="50%" fill="#eceeef" dy=".3em">
-                  Thumbnail
-                </text>
-              </svg>
+            <div class="col-lg-4">
+              <FollowUs />
+
+              <div className="mb-3">
+                <div className="section-title mb-0">
+                  <h4 className="m-0 text-uppercase font-weight-bold">
+                    Advertisement
+                  </h4>
+                </div>
+                <div className="bg-white text-center border border-top-0 p-3">
+                  <Link to="#">
+                    <img
+                      className="img-fluid"
+                      src="img/news-800x500-2.jpg"
+                      alt="Advertisement"
+                    />
+                  </Link>
+                </div>
+              </div>
+
+              <TrendingNews />
+
+              <div className="mb-3">
+                <div className="section-title mb-0">
+                  <h4 className="m-0 text-uppercase font-weight-bold">Tags</h4>
+                </div>
+                <div className="bg-white border border-top-0 p-3">
+                  <div className="d-flex flex-wrap m-n1">
+                    {tags.map((tag, index) => (
+                      <Link
+                        key={index}
+                        to="#"
+                        className="btn btn-sm btn-outline-secondary m-1"
+                      >
+                        {tag}
+                      </Link>
+                    ))}
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="col-sm-6">
-          <div className="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-            <div className="col p-4 d-flex flex-column position-static">
-              <strong className="d-inline-block mb-2 text-success">
-                Design
-              </strong>
-              <h3 className="mb-0">Post title</h3>
-              <div className="mb-1 text-muted">Nov 11</div>
-              <p className="mb-auto">
-                This is a wider card with supporting text below as a natural
-                lead-in to additional content.
-              </p>
-              <Link to="#" className="stretched-link">
-                Continue reading
-              </Link>
-            </div>
-            <div className="col-auto d-none d-lg-block">
-              <svg
-                className="bd-placeholder-img"
-                width="200"
-                height="250"
-                xmlns="http://www.w3.org/2000/svg"
-                role="img"
-                aria-label="Placeholder: Thumbnail"
-                preserveAspectRatio="xMidYMid slice"
-                focusable="false"
-              >
-                <title>Placeholder</title>
-                <rect width="100%" height="100%" fill="#55595c" />
-                <text x="50%" y="50%" fill="#eceeef" dy=".3em">
-                  Thumbnail
-                </text>
-              </svg>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div className="row mb-2">
-        <div className="col-sm-6">
-          <div className="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-            <div className="col p-4 d-flex flex-column position-static">
-              <strong className="d-inline-block mb-2 text-primary">
-                World
-              </strong>
-              <h3 className="mb-0">Featured post</h3>
-              <div className="mb-1 text-muted">Nov 12</div>
-              <p className="card-text mb-auto">
-                This is a wider card with supporting text below as a natural
-                lead-in to additional content.
-              </p>
-              <Link to="#" className="stretched-link">
-                Continue reading
-              </Link>
-            </div>
-            <div className="col-auto d-none d-lg-block">
-              <svg
-                className="bd-placeholder-img"
-                width="200"
-                height="250"
-                xmlns="http://www.w3.org/2000/svg"
-                role="img"
-                aria-label="Placeholder: Thumbnail"
-                preserveAspectRatio="xMidYMid slice"
-                focusable="false"
-              >
-                <title>Placeholder</title>
-                <rect width="100%" height="100%" fill="#55595c" />
-                <text x="50%" y="50%" fill="#eceeef" dy=".3em">
-                  Thumbnail
-                </text>
-              </svg>
-            </div>
-          </div>
-        </div>
-        <div className="col-sm-6">
-          <div className="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-            <div className="col p-4 d-flex flex-column position-static">
-              <strong className="d-inline-block mb-2 text-success">
-                Design
-              </strong>
-              <h3 className="mb-0">Post title</h3>
-              <div className="mb-1 text-muted">Nov 11</div>
-              <p className="mb-auto">
-                This is a wider card with supporting text below as a natural
-                lead-in to additional content.
-              </p>
-              <Link to="#" className="stretched-link">
-                Continue reading
-              </Link>
-            </div>
-            <div className="col-auto d-none d-lg-block">
-              <svg
-                className="bd-placeholder-img"
-                width="200"
-                height="250"
-                xmlns="http://www.w3.org/2000/svg"
-                role="img"
-                aria-label="Placeholder: Thumbnail"
-                preserveAspectRatio="xMidYMid slice"
-                focusable="false"
-              >
-                <title>Placeholder</title>
-                <rect width="100%" height="100%" fill="#55595c" />
-                <text x="50%" y="50%" fill="#eceeef" dy=".3em">
-                  Thumbnail
-                </text>
-              </svg>
-            </div>
-          </div>
-        </div>
-      </div>
-      <br />
-      <div className="row">
-        <div className="col-sm-3">
-          <hr />
-          <h3 className="mb-0">Sports</h3>
-          <p className="mb-auto">
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content.
-          </p>
-          <div className="mb-4 text-muted">2 days ago | football</div>
-
-          <p className="mb-auto">
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content.
-          </p>
-          <div className="mb-4 text-muted">2 days ago | football</div>
-          <p className="mb-auto">
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content.
-          </p>
-          <div className="mb-4 text-muted">2 days ago | football</div>
-        </div>
-
-        <div className="col-sm-3">
-          <hr />
-          <h3 className="mb-0">Bussiness</h3>
-          <p className="mb-auto">
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content.
-          </p>
-          <div className="mb-4 text-muted">2 days ago | Arusha</div>
-          <p className="mb-auto">
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content.
-          </p>
-          <div className="mb-4 text-muted">2 days ago | Arusha</div>
-          <p className="mb-auto">
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content.
-          </p>
-          <div className="mb-4 text-muted">2 days ago | Arusha</div>
-        </div>
-        <div className="col-sm-3">
-          <hr />
-          <h3 className="mb-0">Fashion</h3>
-          <p className="mb-auto">
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content.
-          </p>
-          <div className="mb-4 text-muted">2 days ago | Congo</div>
-          <p className="mb-auto">
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content.
-          </p>
-          <div className="mb-4 text-muted">2 days ago | Congo</div>
-          <p className="mb-auto">
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content.
-          </p>
-          <div className="mb-4 text-muted">2 days ago | Congo</div>
-        </div>
-        <div className="col-sm-3">
-          <hr />
-          <h3 className="mb-0">Tech</h3>
-          <p className="mb-auto">
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content.
-          </p>
-          <div className="mb-4 text-muted">2 days ago | Kenya</div>
-          <p className="mb-auto">
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content.
-          </p>
-          <div className="mb-4 text-muted">2 days ago | Kenya</div>
-          <p className="mb-auto">
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content.
-          </p>
-          <div className="mb-4 text-muted">2 days ago | Kenya</div>
         </div>
       </div>
     </>
